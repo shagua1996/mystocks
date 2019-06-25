3501e69a38bbacb0d60551f7d83691a785ae9dd4
shagua1996 <m18571637320@163.com>
2019-06-25 16:14:37 +0800

    歌词

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './views/app/app';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
-import { LocaleProvider } from 'antd';
-import { Provider } from 'react-redux';
-import store from './store/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./views/app/app";
+import zhCN from "antd/lib/locale-provider/zh_CN";
+import { LocaleProvider } from "antd";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
-let Pro = <Provider store={store}><LocaleProvider locale={zhCN}><App /></LocaleProvider></Provider>
-ReactDOM.render(Pro, document.getElementById('root'));
-
+let Pro = (
+  <Provider store={store}>
+    <LocaleProvider locale={zhCN}>
+      <App />
+    </LocaleProvider>
+  </Provider>
+);
+ReactDOM.render(Pro, document.getElementById("root"));
+/*
+夜里做了一个美丽的梦，梦里李易峰把我抱住
+*/
